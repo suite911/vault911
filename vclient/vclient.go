fac1c5b707999122b4510b46f723c844a81823b8
Amy <amyadzuki@gmail.com>
2018-08-27 19:41:28 +0000

    Update vclient.go

--- vclient/vclient.go
+++ vclient/vclient.go
@@ -10,13 +10,12 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-func Post(url string, message []byte, key [32]byte) (statusCode int, plaintext []bytes, err error) {
+func Post(url string, message []byte, args *fasthttp.Args, key [32]byte) (statusCode int, plaintext []bytes, err error) {
 	var v vault.Vault
 	v.Init(message, key)
-	var args fasthttp.Args
 	args.Set("ts", strconv.Itoa(v.TimeStamp))
 	args.SetBytesV("ct", v.Payload)
-	sc, body, e := fasthttp.Post(nil, url, &args)
+	sc, body, e := fasthttp.Post(nil, url, args)
 	if e == nil && (sc < 200 || sc > 299) {
 		e = errors.New("HTTP "+strconv.Itoa(sc))
 	}
