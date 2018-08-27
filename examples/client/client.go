85a0a26b2792b5c0f64bc17acda63437866adb65
Amy <amyadzuki@gmail.com>
2018-08-27 21:13:30 +0000

    Update client.go

--- examples/client/client.go
+++ examples/client/client.go
@@ -6,17 +6,20 @@ import (
 	"unicode/utf8"
 
 	"github.com/suite911/vault911/vclient"
+	"github.com/suite911/vault911/vkey"
 	"github.com/valyala/fasthttp"
-	"golang.org/x/crypto/argon2"
-	"golang.org/x/crypto/sha3"
 )
 
 func main() {
-	pPW := flag.String("p", "password", "Password to use")
-	pSalt := flag.String("s", "salt", "Per-user salt to use")
-	pURL := flag.String("u", "http://localhost:8080", "Server address")
+	pKeyFile := flag.String("f", "", "Key file")
+	pPW := flag.String("pw", "password", "Password to use")
+	pSalt := flag.String("salt", "salt", "Per-user salt to use")
+	pURL := flag.String("s", "http://localhost:8080", "Server address")
 	flag.Parse()
-	key := sha3.Sum256(argon2.IDKey(*pPW, *pSalt, 1, 64*1024, 4, 32))
+	key, err := vkey.New(*pPW, *pSalt, *pKeyFile)
+	if err != nil {
+		panic(err)
+	}
 	var args fasthttp.Args
 	_, plaintext, err := vclient.Post(*pURL, strings.Join(flag.Args(), " "), &args, key)
 	if err != nil {
