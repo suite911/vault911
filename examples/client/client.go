c6f0dec854a04a9c1d1b91e8ef4a6e9abdc4752b
Amy <amyadzuki@gmail.com>
2018-08-27 21:21:38 +0000

    Update client.go

--- examples/client/client.go
+++ examples/client/client.go
@@ -5,8 +5,7 @@ import (
 	"strings"
 	"unicode/utf8"
 
-	"github.com/suite911/vault911/vclient"
-	"github.com/suite911/vault911/vkey"
+	"github.com/suite911/vault911/vault"
 	"github.com/valyala/fasthttp"
 )
 
@@ -16,12 +15,12 @@ func main() {
 	pSalt := flag.String("salt", "salt", "Per-user salt to use")
 	pURL := flag.String("s", "http://localhost:8080", "Server address")
 	flag.Parse()
-	key, err := vkey.New(*pPW, *pSalt, *pKeyFile)
+	key, err := vault.NewKey(*pPW, *pSalt, *pKeyFile)
 	if err != nil {
 		panic(err)
 	}
 	var args fasthttp.Args
-	_, plaintext, err := vclient.Post(*pURL, strings.Join(flag.Args(), " "), &args, key)
+	_, plaintext, err := vault.Post(*pURL, strings.Join(flag.Args(), " "), &args, key)
 	if err != nil {
 		panic(err)
 	}
