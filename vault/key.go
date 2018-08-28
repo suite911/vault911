ef2cb9a4e2add5328b86b43a7e3e5034e22dd78a
Amy <amyadzuki@gmail.com>
2018-08-28 22:53:40 +0000

    Update key.go

--- vault/key.go
+++ vault/key.go
@@ -2,6 +2,7 @@ package vault
 
 import (
 	"io/ioutil"
+	"unicode/utf8"
 
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/argon2"
@@ -10,13 +11,16 @@ import (
 
 type Key [32]byte
 
-func NewKey(password string, salt []byte, keyFile string) (*Key, error) {
+func NewKey(password, salt []byte, keyFile string) (*Key, error) {
 	k := new(Key)
 	e := k.Init(password, salt, keyFile)
 	return k, e
 }
 
-func (k *Key) Init(password string, salt []byte, keyFile string) error {
+func (k *Key) Init(password, salt []byte, keyFile string) error {
+	if !utf8.Valid(password) {
+		return errors.Wrap(errors.New("password is not valid Unicode"), "utf8.Valid")
+	}
 	a2id := argon2.IDKey(password, salt, 1, 64*1024, 4, len(*k))
 	if len(a2id) != len(*k) {
 		return errors.Wrap(errors.New("wrong length of IDKey"), "argon2.IDKey")
